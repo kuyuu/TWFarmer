5de578c327be02d08d453671d583ab3be90e735b
Veloric Wu <ForFrandsLOL>
2017-02-14 21:23:55 +0800

    A NEW DAY BEGINS

--- TWFarmer/src/main/java/model/FriendDAO.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-public interface FriendDAO {
-	public List<FriendBean> selectByMemberId(int memberId);
-	public FriendBean insert(FreindBean friendBean);
-	public boolean delete(FreindBean friendBean);
-}
